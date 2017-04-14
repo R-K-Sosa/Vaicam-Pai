2c9773759d7304a8bb06d4639c502c42ce0fb806
Drew Ku <andrewku123@gmail.com>
2017-04-13 23:16:16 -0400

    parse json

--- app/src/main/java/com/example/drew/test/MainActivity.java
+++ app/src/main/java/com/example/drew/test/MainActivity.java
@@ -10,6 +10,17 @@ import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends YouTubeBaseActivity {
     YouTubePlayerView youtubePlayerView;
     Button button;
@@ -40,5 +51,31 @@ public class MainActivity extends YouTubeBaseActivity {
             }
         });
 
+    }
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, JSONException {
+        JSONObject json = readJsonFromUrl("https://105ba16f.ngrok.io/");
+        System.out.println(json.toString());
+        System.out.println(json.get("id"));
     }
 }
